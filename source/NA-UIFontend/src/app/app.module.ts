78a4e0b1c3f6f1941ca815beefe9c2a7331bc9f0
bigo92 <ducnv.viss@gmail.com>
2019-08-09 11:07:54 +0700

    add ant design

--- source/NA-UIFontend/src/app/app.module.ts
+++ source/NA-UIFontend/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+
+/** config angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -11,9 +18,13 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule,
     AppRoutes
   ],
-  providers: [],
+  providers   : [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- source/NA-UIFontend/src/app/layout/home/home.module.ts
+++ source/NA-UIFontend/src/app/layout/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutes } from './home.routing';
+import { NzIconModule } from 'ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
+    NzIconModule,
     HomeRoutes
   ],
   declarations: [HomeComponent]
