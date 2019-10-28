a246f6fa6ac8ed71a58de651907d723dcb30b893
nguyen van duc <ducnv.viss@gmail.com>
2019-10-28 13:58:17 +0700

    remove ui home

--- source/NA-UIFontend/src/app/layout/home/home.module.ts
+++ source/NA-UIFontend/src/app/layout/home/home.module.ts
@@ -2,17 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutes } from './home.routing';
-import { NzIconModule, NzModalModule } from 'ng-zorro-antd';
-import { FormModule } from 'src/app/_base/modules/form/form.module';
-import { HomeDialogModule } from './home-dialog/home-dialog.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    NzIconModule,
-    FormModule,
-    NzModalModule,
-    HomeDialogModule,
     HomeRoutes
   ],
   declarations: [HomeComponent]
