e1c1607bc2f6d6e5fea44f5456c67ee4239ae9b8
nguyen van duc <ducnv.viss@gmail.com>
2019-11-05 17:32:27 +0700

    fix

--- source/NA-UIFontend/src/app/layout/home/home.module.ts
+++ source/NA-UIFontend/src/app/layout/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutes } from './home.routing';
+import { FormModule } from 'src/app/_base/modules/form/form.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormModule,
     HomeRoutes
   ],
   declarations: [HomeComponent]
